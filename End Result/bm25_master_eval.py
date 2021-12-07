1b216775ad8d0a0b1a84da0c08b75f998b2b86e4
Zurawski, Alexander R <azuraws2@illinois.edu>
2021-12-07 16:53:42 -0600

    Fixed bm25_eval cd desktop/cs410/courseproject - Copy/Reproduce Results

--- End Result/bm25_master_eval.py
+++ End Result/bm25_master_eval.py
@@ -4,9 +4,6 @@
 
 import bm25_eval
 
-# Load configuration file
-cfg = 'config.toml'
-
 # Initialize values
 best = 0
 k1 = 0
@@ -21,7 +18,7 @@ for i in range(1, 200, 1):
         # i and j are divided by 100 to test decimals
 
         # Run bm25_eval with chosen parameters
-        new = bm25_eval.bm25_eval(cfg, i / 100, j / 100)
+        new = bm25_eval.bm25_eval(i / 100, j / 100)
 
         # If the score is better than the previous best, replace parameters
         if new > best:
@@ -32,4 +29,4 @@ for i in range(1, 200, 1):
 # Print out best score and its associated parameters
 # Could be better automated in the future
 print(best)
-print(str(k1) + ' ' + str(b))
+print('k1 =' + str(k1) + ' ' + 'b = ' + str(b))
